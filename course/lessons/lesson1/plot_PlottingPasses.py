2bd1d5ce662bd7b115103933cfc2e69f4f5c3c2c
oleka222 <62180109+oleka222@users.noreply.github.com>
2022-08-17 13:55:13 +0200

    plot updates

--- course/lessons/lesson1/plot_PlottingPasses.py
+++ course/lessons/lesson1/plot_PlottingPasses.py
@@ -1,28 +1,83 @@
-""""
-# TO DO ALEKSANDER: DO A PLOT THE SHOTS VERSION USING 'QUERY' INSTEAD
-# ALSO USE MPL SOCCER INSTEAD OF CURRENT VERSION.
-# ALSO, CHECK THIS IS RIGHT DIRECTIONS. I THINK I GOT SOME THINGS WRONG.
-"""
+import matplotlib.pyplot as plt
+import numpy as np
+from pandas.io.json import json_normalize
+from mplsoccer import Pitch
+import json
 
-#Find the passes
+pitchLengthX=120
+pitchWidthY=80
+
+match_id_required = 69301
+home_team_required ="England Women's"
+away_team_required ="Sweden Women's"
+
+# Load in the data
+# I took this from https://znstrider.github.io/2018-11-11-Getting-Started-with-StatsBomb-Data/
+file_name=str(match_id_required)+'.json'
+
+#Load in all match events 
+import json
+with open('Statsbomb/data/events/'+file_name) as data_file:
+    #print (mypath+'events/'+file)
+    data = json.load(data_file)
+
+#get the nested structure into a dataframe 
+#store the dataframe in a dictionary with the match id as key (remove '.json' from string)
+from pandas.io.json import json_normalize
+df = json_normalize(data, sep = "_").assign(match_id = file_name[:-5])
+
+#A dataframe of shots
 passes = df.loc[df['type_name'] == 'Pass'].set_index('id')
 
-#Draw the pitch
-(fig,ax) = createPitch(pitchLengthX,pitchWidthY,'yards','gray')
+# Draw the pitch, record that Statsbomb is the default value of Pitch class. Each provider uses different coordinate system.
+# Useful links
+# https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_setup/plot_pitches.html
+# https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_setup/plot_compare_pitches.html
+pitch = Pitch(line_color = "black")
+fig, ax = pitch.draw(figsize=(10, 7))
+
 for i,thepass in passes.iterrows():
     #if thepass['team_name']==away_team_required: #
     if thepass['player_name']=='Lucy Bronze':
         x=thepass['location'][0]
         y=thepass['location'][1]
-        passCircle=plt.Circle((x,pitchWidthY-y),2,color="blue")      
+        passCircle=plt.Circle((x,y),2,color="blue")      
         passCircle.set_alpha(.2)   
         ax.add_patch(passCircle)
         dx=thepass['pass_end_location'][0]-x
         dy=thepass['pass_end_location'][1]-y
 
-        passArrow=plt.Arrow(x,pitchWidthY-y,dx,-dy,width=3,color="blue")
+        passArrow=plt.Arrow(x,y,dx,dy,width=3,color="blue")
         ax.add_patch(passArrow)
 
+ax.set_title("Lucy Bronze passes against Sweden", fontsize = 20) 
+     
 fig.set_size_inches(10, 7)
-fig.savefig('Output/passes.pdf', dpi=100) 
+#fig.savefig('Output/shots.pdf', dpi=100) 
+plt.show()
+
+
+###
+
+#Plotting it using mplsoccer Statsbomb parser, without looping through rows
+from mplsoccer import Pitch, Sbopen
+
+
+# read data using StatsBomb parser
+parser = Sbopen()
+df, related, freeze, tactics = parser.event(69301)
+
+
+# filter the dataset to completed passes for Lucy Bronze
+mask_bronze = (df.type_name == 'Pass') & (df.player_name == "Lucy Bronze")
+df_pass = df.loc[mask_bronze, ['x', 'y', 'end_x', 'end_y']]
+
+pitch = Pitch(pitch_color='grass', line_color='white', stripe=True)
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+pitch.arrows(df_pass.x, df_pass.y,
+            df_pass.end_x, df_pass.end_y, color = "blue", ax=ax['pitch'])
+pitch.scatter(df_pass.x, df_pass.y, alpha = 0.2, s = 500, color = "blue", ax=ax['pitch'])
+#fig.savefig('Output/shots.pdf', dpi=100)
+fig.suptitle("Lucy Bronze passes against Sweden", fontsize = 20) 
 plt.show()
--- course/lessons/lesson1/plot_PlottingShots.py
+++ course/lessons/lesson1/plot_PlottingShots.py
@@ -7,10 +7,8 @@ Hello
 #Function to draw the pitch
 import matplotlib.pyplot as plt
 import numpy as np
+from mplsoccer import Pitch, Sbopen, VerticalPitch
 
-#Size of the pitch in yards (!!!)
-pitchLengthX=120
-pitchWidthY=80
 
 #ID for England vs Sweden Womens World Cup
 match_id_required = 69301
@@ -35,11 +33,18 @@ df = json_normalize(data, sep = "_").assign(match_id = file_name[:-5])
 #A dataframe of shots
 shots = df.loc[df['type_name'] == 'Shot'].set_index('id')
     
-#Draw the pitch
-#TO DO ALEKSANDER: REPLACE THE PLOT PITCH WITH MPL SOCCER PLOT PITCH.
 
-from FCPython import createPitch
-(fig,ax) = createPitch(pitchLengthX,pitchWidthY,'yards','gray')
+# Draw the pitch, record that Statsbomb is the default value of Pitch class. Each provider uses different coordinate system.
+# Useful links
+# https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_setup/plot_pitches.html
+# https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_setup/plot_compare_pitches.html
+
+#Size of the pitch in yards (!!!)
+pitchLengthX=120
+pitchWidthY=80
+
+pitch = Pitch(line_color = "black")
+fig, ax = pitch.draw(figsize=(10, 7))
 
 
 #Plot the shots by looping through them.
@@ -55,31 +60,84 @@ for i,shot in shots.iterrows():
 
     if (team_name==home_team_required):
         if goal:
-            shotCircle=plt.Circle((x,pitchWidthY-y),circleSize,color="red")
-            plt.text((x+1),pitchWidthY-y+1,shot['player_name']) 
+            shotCircle=plt.Circle((x,y),circleSize,color="red")
+            plt.text(x+1,y-2,shot['player_name']) 
         else:
-            shotCircle=plt.Circle((x,pitchWidthY-y),circleSize,color="red")     
+            shotCircle=plt.Circle((x,y),circleSize,color="red")     
             shotCircle.set_alpha(.2)
     elif (team_name==away_team_required):
         if goal:
-            shotCircle=plt.Circle((pitchLengthX-x,y),circleSize,color="blue") 
-            plt.text((pitchLengthX-x+1),y+1,shot['player_name']) 
+            shotCircle=plt.Circle((pitchLengthX-x,pitchWidthY - y),circleSize,color="blue") 
+            plt.text(pitchLengthX-x+1,pitchWidthY - y -2 ,shot['player_name']) 
         else:
-            shotCircle=plt.Circle((pitchLengthX-x,y),circleSize,color="blue")      
+            shotCircle=plt.Circle((pitchLengthX-x,pitchWidthY - y),circleSize,color="blue")      
             shotCircle.set_alpha(.2)
     ax.add_patch(shotCircle)
 
-plt.text(5,75,away_team_required + ' shots') 
-plt.text(80,75,home_team_required + ' shots') 
-     
+
+fig.suptitle("England (red) and Sweden (blue) shots", fontsize = 20)     
 fig.set_size_inches(10, 7)
-fig.savefig('Output/shots.pdf', dpi=100) 
+#fig.savefig('Output/shots.pdf', dpi=100) 
 plt.show()
 
-#TO DO ALEKSANDER: DO A PLOT THE SHOTS VERSION USING 'QUERY' INSTEAD
+#Plotting it without looping through rows and using Statsbomb parser
+#Reading Statsbomb data using the parser - 69301 is the id of the game in the database
+# Record that Statsbomb is the default value of Pitch class 'pitch_type' variable. Each provider uses different coordinate system.
+# Useful links
+# https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_setup/plot_pitches.html
+# https://mplsoccer.readthedocs.io/en/latest/gallery/pitch_setup/plot_compare_pitches.html
+parser = Sbopen()
+df, related, freeze, tactics = parser.event(69301)
+
+#create pitch - let's explore more options using mplsoccer than black and white pitch
+pitch = Pitch(pitch_color='grass', line_color='white', stripe=True)
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+
+#make a query to get a dataframe with England shots
+team1, team2 = df.team_name.unique()
+#query
+mask_england = (df.type_name == 'Shot') & (df.team_name == team1)
+#finding rows in the df and keeping only necessary columns
+df_england = df.loc[mask_england, ['x', 'y', 'outcome_name', "player_name"]]
+
+#plot them - if shot ended with Goal - alpha 1 and add name
+#for England
+for i, row in df_england.iterrows():
+    if row["outcome_name"] == 'Goal':
+       pitch.scatter(row.x, row.y, alpha = 1, s = 500, color = "red", ax=ax['pitch']) 
+       pitch.annotate(row["player_name"], (row.x + 1, row.y - 2), ax=ax['pitch'], fontsize = 12)
+    else: 
+       pitch.scatter(row.x, row.y, alpha = 0.2, s = 500, color = "red", ax=ax['pitch']) 
+       
+mask_sweden = (df.type_name == 'Shot') & (df.team_name == team2)
+df_sweden = df.loc[mask_sweden, ['x', 'y', 'outcome_name', "player_name"]]   
+
+#for sweden we need to revert coordinates
+for i, row in df_sweden.iterrows():
+    if row["outcome_name"] == 'Goal':
+       pitch.scatter(120 - row.x, 80 - row.y, alpha = 1, s = 500, color = "blue", ax=ax['pitch']) 
+       pitch.annotate(row["player_name"], (120 - row.x + 1, 80 - row.y - 2), ax=ax['pitch'], fontsize = 12)
+    else: 
+       pitch.scatter(120 - row.x, 80 - row.y, alpha = 0.2, s = 500, color = "blue", ax=ax['pitch']) 
+       
+fig.suptitle("England (red) and Sweden (blue) shots", fontsize = 20)           
+#fig.savefig('Output/shots.pdf', dpi=100)
+plt.show()
 
 
 
+
+#Note that using pitch.scatter we could have plotted all shots using one line, however, since name of a player and alpha differs if goal was scored, it was more convenient to loop through a small loop.
+#Showing it using VerticalPitch for England, half = True plots only one half of the pitch, nice option if you are plotting shots
+pitch = VerticalPitch(pitch_color='grass', line_color='white', stripe=True, half = True)
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+#plotting all shots
+pitch.scatter(df_england.x, df_england.y, alpha = 1, s = 500, color = "red", ax=ax['pitch'], edgecolors="black") 
+fig.suptitle("England shots against Sweden", fontsize = 20)           
+#fig.savefig('Output/shots.pdf', dpi=100)
+plt.show()
 
 #Exercise:
 
