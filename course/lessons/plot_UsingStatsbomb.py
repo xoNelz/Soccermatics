e7ebcb8ff9d197f4bc6313a7e591c09eb04255b6
oleka222 <62180109+oleka222@users.noreply.github.com>
2022-08-25 21:00:26 +0200

    added uding data

--- course/lessons/plot_UsingStatsbomb.py
+++ course/lessons/plot_UsingStatsbomb.py
@@ -4,62 +4,81 @@ Using Statsbomb
 Getting familiar with Statsbomb data
 """
 
-#CODE WAITING FOR FILE CONFIRMATION - until then it's a total mess
-#importing necessary libraries
-
-import pandas as pd
+#importing SBopen class from mplsoccer to open the data
 from mplsoccer import Sbopen
-#from tabulate import tabulate
-#declaring the parser to open the data
+# The first thing we have to do is open the data. We use a parser SBopen available in mplsoccer.
 parser = Sbopen()
-#first open and explore competitions to find the competition we are interested in exploring - we open it using competition method
 
+##############################################################################
+# Competition data
+# ----------------------------
+# Using method *competition* of the parser we can explore competitions to find the competition we are interested in.
+# The most important information for us is in the *competition_id* (id of competition) and *season_id*.
+# The first one is the key in Statsbomb database of a competition, the second one of a season 
+# of this competition (for example WC 2018 would have a different *season_id* than WC 2014, but the same *competition_id*).
+
+#opening data using competition method
 df_competition = parser.competition()
-#explore structure - print out 3 first records and info (if there are more columns)
-df_competition.head(3)
-#structure (if more columns and not all were shown previously)
+#structure of data
 df_competition.info()
+#explore structure - print out 3 first records and column names
+df_competition.head(3)
 
-# the most important information for us is in the *competition_id* (id of competition) and *season_id*
-# the first one is id in Statsbomb database of competition, the second one of a season of this competition (for example 
-# WC 2018 would have a different *season_id* than WC 2014, but the same *competition_id*)
 
-# now we open the dataframe with matches
-# we know (but if you can also open the entire competitions dataframe and check *competition_id*  and *season_id*
-# of different leagues) that for Women World Cup *competition_id* is 72 and *season_id* is 30
+##############################################################################
+# Match data
+# ----------------------------
+# Using method *method* of the parser we can explore matches of a competition to find the match we are interested in.
+# To open it we need to know the *competition_id* (id of competition) and *season_id*.
+# We know that for Women World Cup *competition_id* is 72 and *season_id* is 30
+# From this dataframe for us the most important imformation is provided in *match_id*, 
+# *home_team_id* and *home_team_name* and adequately *away_team_id* and *away_team_name*.
+
+#opening data using match method
 df_match = parser.match(competition_id=72, season_id=30)
-df_match.head(3)
+#structure of data
 df_match.info()
+#explore structure - print out 3 first records and column names
+df_match.head(3)
 
-# from this dataframe for us the most important are *match_id*, *home_team_id* and *home_team_name* and adequately 
-# *away_team_id* and *away_team_name* 
 
+##############################################################################
+# Match data
+# ----------------------------
 # To check the lineups we use the *lineup* method. We do it for England Sweden WWC 2019 game - *game_id* is 69301 
 # - you can check that in the df_match. In this dataframe you will find all players who played in this game, their teams 
 # and jersey numbers
-df_lineup = parser.lineup(69301)
-df_lineup.head(3)
-df_lineup.info()
 
+#opening data using match method
+df_lineup = parser.lineup(69301)
+#structure of data
+df_lineup.info()
+#explore structure - print out 3 first records and column names
+df_lineup.head(3)
+
+##############################################################################
+# Match data
+# ----------------------------
 # The Statsbomb data that we will use the most during the course is event data. 
 # Knowing *game_id* you can open all the events that occured on the pitch
 # In the event dataframe you will find events with additional information, we will mostly use this dataframe.
 # Tactics dataframe provides information about player position on the pitch. 'Related' dataframe provides information
 # on events that were related to each other - for example ball pass and pressure applied. *df_freeze* consists of freezed
-# frames with player position in the moment of shots. We will learn more about tracking data later in the course
+# frames with player position in the moment of shots. We will learn more about tracking data later in the course.
+# Below, an example of event data is presented.
 
+#opening data
 df_event, df_related, df_freeze, df_tactics = parser.event(69301)
-
-# exploring the data
-df_event.head(3)
+#if you want only event data you can use 
+#df_event = parser.event(69301)[0]
+#structure of data
 df_event.info()
-df_related.head(3)
-df_related.info()
-df_freeze.head(3)
-df_freeze.info()
-df_tactics.head(3)
-df_tactics.info()
+#explore structure - print out 3 first records and column names
+df_event.head(3)
 
+##############################################################################
+# 360 data
+# ----------------------------
 # Statsbomb offers 360 data which track not only location of an event but also players' location. To open them we need
 # an id of freezed frame. In the *df_frame* we find information on players' position (but only if teammate, not all information)
 # and in *df_visible* it is provided which part of the pitch was tracked during an event.
@@ -69,9 +88,9 @@ df_frame, df_visible = parser.frame(3788741)
 # exploring the data
 df_frame.info()
 df_frame.head(3)
-df_visible.info()
-df_visible.head(3)
 
-# Before you start 
-# ---
-# run this lines in Spyder/Jupyter notebook and explore dataframes more to get more familiar before you start working on the course
+##############################################################################
+# Before you start
+# ----------------------------
+# Run these lines in Spyder/Jupyter notebook and explore dataframes 
+# to get more familiar before you start working on the course.
--- course/lessons/plot_UsingWyscout.py
+++ course/lessons/plot_UsingWyscout.py
@@ -5,51 +5,76 @@ Using Wyscout
 Getting familiar with Wyscout data
 """
 #CODE WAITING FOR FILE CONFIRMATION - until then it's a total mess
-#importing necessary libraries
 
-# If you are trying it locally, comment  ..... active lines (put # in front lines path = ...) and comment out (delete #)
-#commented out lines
+
+#importing necessary libraries
 import pathlib
 import os
 import pandas as pd
 import json
 
 
-# Competitions dataframe 
-# In this dataframe you will find information about the id of a competition and available competitions 
+##############################################################################
+# Competition data
+# ----------------------------
+# In this dataframe you will find information about the id of a competition and available competitions.
+# If you are trying it locally, comment  ..... active lines (put # in front lines path = ...) and comment out (delete #).
+
+#path to data
 path = os.path.join(str(pathlib.Path().resolve()), 'data', 'Wyscout', 'competitions.json') # put # in front if used locally
 #path = os.path.join(str(pathlib.Path().resolve()), 'Wyscout', 'competitions.json') # delete #
 
+#open data 
 with open(path) as f:
     data = json.load(f)
+    #save it in dataframe 
 df_competitions = pd.DataFrame(data)
-df_competitions.head(3)
+#structure of data
 df_competitions.info()
+#explore structure - print out 3 first records and column names
+df_competitions.head(3)
 
-# Matches dataframe 
+##############################################################################
+# Match data
+# ----------------------------
 # In this dataframe you can find information about all games that were played in Premier League 2017/18 season.
-#*wyId* is the unique id in the Wyscout database. 
+# *wyId* is the unique id in the Wyscout database. 
+
+#path to data 
 path = os.path.join(str(pathlib.Path().resolve()), 'data', 'Wyscout', 'matches_England.json') # put # in front if used locally
 #path = os.path.join(str(pathlib.Path().resolve()), 'data', 'Wyscout', 'matches_England.json') # delete #
 with open(path) as f:
     data = json.load(f)
+#save it in a dataframe  
 df_matches = pd.DataFrame(data)
-df_matches.head(3)
+#structure of data
 df_matches.info()
+#explore structure - print out 3 first records and column names
+df_matches.head(3)
 
-#Players dataframe
+##############################################################################
+# Player data
+# ----------------------------
 # In this dataframe you can find information about all players available for Wyscout public dataset. *wyId* is 
 # the player id in the Wyscout database. In the *currentTeamId* you can find the id of a team that the player plays form.
-#*shortName* is an important column for vizualisations and rankings since player's name is written in a shorter way.
+# *shortName* is an important column for vizualisations and rankings since player's name is written in a shorter way.
+
+#path to data
 path = os.path.join(str(pathlib.Path().resolve()), 'data', 'Wyscout', 'players.json') # put # in front if used locally
 #path = os.path.join(str(pathlib.Path().resolve()), 'data', 'Wyscout', 'players.json')  
+#open data
 with open(path) as f:
     data = json.load(f)
+#save it in a dataframe  
 df_players = pd.DataFrame(data)
-df_players.head(3)
+#structure of data
 df_players.info()
+#explore structure - print out 3 first records and column names
+df_players.head(3)
 
-#Events dataframe
+##############################################################################
+# Event data
+# ----------------------------
 # In this dataframe you can find information about all events that occured in all the games during 2017/18 Premier League
 # season. *matchId* matches the wyId from *df_matches*, *playerId* matches *wyId* from *df_players*.*tags* provide information on additional characteristics of an event, for example
 # if the pass was accurate. The location on the pass can be found in *positions*, but remeber, that the data are collected
@@ -59,16 +84,19 @@ df_players.info()
 # If you want to learn more about Wyscout data, you can explore 
 # `WyScout API <https://apidocs.wyscout.com/>`_, but remember to switch the version to 2.0 at the top of the page.
 # 
-#
 # This code is adjusted to the webpage with file size limit. If you want to open the data that is stored in the working
 # directory, comment (put '#') before the following code and comment out the lines below them (delete '#').
 
+#prepare empty dataframe
 df_events = pd.DataFrame() # put # in front if used locally
 for i in range(13): # put # in front if used locally
+    #get file name and path to it
     file_name = 'events_England_' + str(i+1) + '.json' # put # in front if used locally
     path = os.path.join(str(pathlib.Path().resolve()), 'data', 'Wyscout', file_name) # put # in front if used locally
+    #open data
     with open(path) as f: # put # in front if used locally
         data = json.load(f) # put # in front if used locally
+    #append data to the dataframe
     df_events = pd.concat([df_events, pd.DataFrame(data)]) # put # in front if used locally
 
 
@@ -77,5 +105,13 @@ for i in range(13): # put # in front if used locally
     #data = json.load(f) # delete #
 #df_events = pd.DataFrame(data) # delete #
 
-df_events.head(3)
+#structure of data
 df_events.info()
+#explore structure - print out 3 first records and column names
+df_players.head(3)
+
+##############################################################################
+# Before you start
+# ----------------------------
+# Run these lines in Spyder/Jupyter notebook and explore dataframes 
+# to get more familiar before you start working on the course.
