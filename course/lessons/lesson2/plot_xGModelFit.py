e5ed6915bfbb4d2768f6654c39848ce46061bff8
oleka222 <62180109+oleka222@users.noreply.github.com>
2022-08-27 17:20:22 +0200

    xg model update

--- course/lessons/lesson2/plot_xGModelFit.py
+++ course/lessons/lesson2/plot_xGModelFit.py
@@ -13,13 +13,14 @@ import json
 
 # Plotting
 import matplotlib.pyplot as plt
-import FCPython
 
 # Statistical fitting of models
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import os
 import pathlib
+import FCPython
+from mplsoccer import VerticalPitch
 
 # Decide which league to load
 # Wyscout data from https://figshare.com/collections/Soccer_match_event_dataset/4415000/2
@@ -30,87 +31,92 @@ for i in range(13):
     with open(path) as f:
         data = json.load(f)
     train = pd.concat([train, pd.DataFrame(data)])
+    
+    
+    
+    
 # Create a data set of shots.
-
-pd.unique(train['subEventName'])
-shots = train[train['subEventName'] == 'Shot']
-shots_model = pd.DataFrame(columns=['Goal', 'X', 'Y'])
-
+shots = train.loc[train['subEventName'] == 'Shot']
+shots["X"] = shots.positions.apply(lambda cell: (100 - cell[0]['x']) * 105/100)
+shots["Y"] = shots.positions.apply(lambda cell: cell[0]['y'] * 68/100)
+shots["C"] = shots.positions.apply(lambda cell: abs(cell[0]['y'] - 50) * 68/100)
+shots["Distance"] = np.sqrt(shots["X"]**2 + shots["C"]**2)
+shots["Angle"] = np.where(np.arctan(7.32 * shots["X"] / (shots["X"]**2 + shots["C"]**2 - (7.32/2)**2)) > 0, np.arctan(7.32 * shots["X"] /(shots["X"]**2 + shots["C"]**2 - (7.32/2)**2)), np.arctan(7.32 * shots["X"] /(shots["X"]**2 + shots["C"]**2 - (7.32/2)**2)) + np.pi)
+#if you ever encounter problems (like you have seen that model treats 0 as 1 and 1 as 0) while modelling - change the dependant variable to object 
+shots["Goal"] = shots.tags.apply(lambda x: 1 if {'id':101} in x else 0).astype(object)
 # Go through the dataframe and calculate X, Y co-ordinates.
 # Distance from a line in the centre
 # Shot angle.
 # Details of tags can be found here: https://apidocs.wyscout.com/matches-wyid-events
-for i, shot in shots.iterrows():
+#for i, shot in shots.iterrows():
 
-    header = 0
-    for shottags in shot['tags']:
-        if shottags['id'] == 403:
-            header = 1
+  #  header = 0
+  #  for shottags in shot['tags']:
+  #      if shottags['id'] == 403:
+   #         header = 1
     # Only include non-headers
-    if not (header):
-        shots_model.at[i, 'X'] = 100 - shot['positions'][0]['x']
-        shots_model.at[i, 'Y'] = shot['positions'][0]['y']
-        shots_model.at[i, 'C'] = abs(shot['positions'][0]['y'] - 50)
+  #  if not (header):
+  #      shots_model.at[i, 'X'] = 100 - shot['positions'][0]['x']
+  #      shots_model.at[i, 'Y'] = shot['positions'][0]['y']
+   #     shots_model.at[i, 'C'] = abs(shot['positions'][0]['y'] - 50)
 
         # Distance in metres and shot angle in radians.
-        x = shots_model.at[i, 'X'] * 105 / 100
-        y = shots_model.at[i, 'C'] * 65 / 100
-        shots_model.at[i, 'Distance'] = np.sqrt(x ** 2 + y ** 2)
-        a = np.arctan(7.32 * x / (x ** 2 + y ** 2 - (7.32 / 2) ** 2))
-        if a < 0:
-            a = np.pi + a
-        shots_model.at[i, 'Angle'] = a
-
+    #    x = shots_model.at[i, 'X'] * 105 / 100
+    #    y = shots_model.at[i, 'C'] * 65 / 100
+     #   shots_model.at[i, 'Distance'] = np.sqrt(x ** 2 + y ** 2)
+     #   a = np.arctan(7.32 * x / (x ** 2 + y ** 2 - (7.32 / 2) ** 2))
+     #   if a < 0:
+     #       a = np.pi + a
+     #   shots_model.at[i, 'Angle'] = a
+#
         # Was it a goal
-        shots_model.at[i, 'Goal'] = 0
-        for shottags in shot['tags']:
+     #   shots_model.at[i, 'Goal'] = 0
+     #   for shottags in shot['tags']:
             # Tags contain that its a goal
-            if shottags['id'] == 101:
-                shots_model.at[i, 'Goal'] = 1
+      #      if shottags['id'] == 101:
+       #         shots_model.at[i, 'Goal'] = 1
 
 # Two dimensional histogram
-H_Shot = np.histogram2d(shots_model['X'], shots_model['Y'], bins=50, range=[[0, 100], [0, 100]])
-goals_only = shots_model[shots_model['Goal'] == 1]
-H_Goal = np.histogram2d(goals_only['X'], goals_only['Y'], bins=50, range=[[0, 100], [0, 100]])
 
-# Plot the number of shots from different points
-(fig, ax) = FCPython.createGoalMouth()
-pos = ax.imshow(H_Shot[0], extent=[-1, 66, 104, -1], aspect='auto', cmap=plt.cm.Reds)
-fig.colorbar(pos, ax=ax)
-ax.set_title('Number of shots')
-plt.xlim((-1, 66))
-plt.ylim((-3, 35))
-plt.tight_layout()
-plt.gca().set_aspect('equal', adjustable='box')
+pitch = VerticalPitch(line_color='black', half = True, pitch_type='custom', pitch_length=105, pitch_width=68, line_zorder = 2)
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+bin_statistic_shots = pitch.bin_statistic(105 - shots.X, shots.Y, bins=50)
+pcm = pitch.heatmap(bin_statistic_shots, ax=ax["pitch"], cmap='Reds', edgecolor='white', linewidth = 0.01)
+ax_cbar = fig.add_axes((0.95, 0.05, 0.04, 0.8))
+cbar = plt.colorbar(pcm, cax=ax_cbar)
+fig.suptitle('Shot map - 2017/2018 Premier League Season' , fontsize = 30)
 plt.show()
 
-# Plot the number of GOALS from different points
-(fig, ax) = FCPython.createGoalMouth()
-pos = ax.imshow(H_Goal[0], extent=[-1, 66, 104, -1], aspect='auto', cmap=plt.cm.Reds)
-fig.colorbar(pos, ax=ax)
-ax.set_title('Number of goals')
-plt.xlim((-1, 66))
-plt.ylim((-3, 35))
-plt.tight_layout()
-plt.gca().set_aspect('equal', adjustable='box')
+goals = shots.loc[shots["Goal"] == 1]
+pitch = VerticalPitch(line_color='black', half = True, pitch_type='custom', pitch_length=105, pitch_width=68, line_zorder = 2)
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+bin_statistic_goals = pitch.bin_statistic(105 - goals.X, goals.Y, bins=50)
+pcm = pitch.heatmap(bin_statistic_goals, ax=ax["pitch"], cmap='Reds', edgecolor='white')
+ax_cbar = fig.add_axes((0.95, 0.05, 0.04, 0.8))
+cbar = plt.colorbar(pcm, cax=ax_cbar)
+fig.suptitle('Goal map - 2017/2018 Premier League Season' , fontsize = 30)
 plt.show()
+
 
 # Plot the probability of scoring from different points
-(fig, ax) = FCPython.createGoalMouth()
-pos = ax.imshow(H_Goal[0] / H_Shot[0], extent=[-1, 66, 104, -1], aspect='auto', cmap=plt.cm.Reds, vmin=0, vmax=0.5)
-fig.colorbar(pos, ax=ax)
-ax.set_title('Proportion of shots resulting in a goal')
-plt.xlim((-1, 66))
-plt.ylim((-3, 35))
-plt.tight_layout()
-plt.gca().set_aspect('equal', adjustable='box')
+pitch = VerticalPitch(line_color='black', half = True, pitch_type='custom', pitch_length=105, pitch_width=68, line_zorder = 2)
+fig, ax = pitch.grid(grid_height=0.9, title_height=0.06, axis=False,
+                     endnote_height=0.04, title_space=0, endnote_space=0)
+bin_statistic = pitch.bin_statistic(105 - shots.X, shots.Y, bins = 50)
+bin_statistic["statistic"] = bin_statistic_goals["statistic"]/bin_statistic["statistic"]
+pcm = pitch.heatmap(bin_statistic, ax=ax["pitch"], cmap='Reds', edgecolor='white', vmin = 0, vmax = 0.6)
+ax_cbar = fig.add_axes((0.95, 0.05, 0.04, 0.8))
+cbar = plt.colorbar(pcm, cax=ax_cbar)
+fig.suptitle('Probability of scoring' , fontsize = 30)
 plt.show()
 
 #Plot a logistic curve
-b=[3, -3]
-x=np.arange(5,step=0.1)
-y=1/(1+np.exp(-b[0]-b[1]*x))
-fig,ax=plt.subplots(num=1)
+b = [3, -3]
+x = np.arange(5, step=0.1)
+y = 1/(1+np.exp(-b[0]-b[1]*x))
+fig,ax = plt.subplots()
 plt.ylim((-0.05,1.05))
 plt.xlim((0,5))
 ax.set_ylabel('y')
@@ -121,10 +127,10 @@ ax.spines['right'].set_visible(False)
 plt.show()
 
 #Get first 200 shots
-shots_200=shots_model.iloc[:200]
+shots_200=shots.iloc[:200]
 
 #Plot first 200 shots goal angle
-fig,ax=plt.subplots(num=1)
+fig, ax = plt.subplots()
 ax.plot(shots_200['Angle']*180/np.pi, shots_200['Goal'], linestyle='none', marker= '.', markersize= 12, color='black')
 ax.set_ylabel('Goal scored')
 ax.set_xlabel("Shot angle (degrees)")
@@ -134,18 +140,19 @@ ax.set_yticklabels(['No','Yes'])
 plt.show()
 
 #Show empirically how goal angle predicts probability of scoring
-shotcount_dist=np.histogram(shots_model['Angle']*180/np.pi,bins=40,range=[0, 150])
-goalcount_dist=np.histogram(goals_only['Angle']*180/np.pi,bins=40,range=[0, 150])
-prob_goal=np.divide(goalcount_dist[0],shotcount_dist[0])
-angle=shotcount_dist[1]
-midangle= (angle[:-1] + angle[1:])/2
-fig,ax=plt.subplots(num=2)
+shotcount_dist = np.histogram(shots['Angle']*180/np.pi, bins=40, range=[0, 150])
+goalcount_dist = np.histogram(goals['Angle']*180/np.pi, bins=40, range=[0, 150])
+np.seterr(divide='ignore', invalid='ignore')
+prob_goal = np.divide(goalcount_dist[0], shotcount_dist[0])
+angle = shotcount_dist[1]
+midangle = (angle[:-1] + angle[1:])/2
+fig,ax = plt.subplots()
 ax.plot(midangle, prob_goal, linestyle='none', marker= '.', markersize= 12, color='black')
 ax.set_ylabel('Probability chance scored')
 ax.set_xlabel("Shot angle (degrees)")
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
-
+plt.show()
 # Code commented out here, because only for illustrative purposes.
 #Try some models by hand
 #First try a linear model
@@ -159,34 +166,35 @@ ax.spines['right'].set_visible(False)
 #Now try sigmoid model
 #This is a good model but NOT a good way of fitting.
 #because each point contains lots of data points
-b=[3, -3]
-x=np.arange(150,step=0.1)
-y=1/(1+np.exp(b[0]+b[1]*x*np.pi/180)) 
+fig, ax = plt.subplots()
+b = [3, -3]
+x = np.arange(150,step=0.1)
+y = 1/(1+np.exp(b[0]+b[1]*x*np.pi/180)) 
 ax.plot(x, y, linestyle='solid', color='black')
 plt.show()
 
 #Now lets look at the likelihood of model given data
-xG=1/(1+np.exp(b[0]+b[1]*shots_model['Angle'])) 
-shots_model = shots_model.assign(xG=xG)
-shots_40=shots_model.iloc[:40]
-fig,ax=plt.subplots(num=1)
-ax.plot(shots_40['Angle']*180/np.pi, shots_40['Goal'], linestyle='none', marker= '.', markersize= 12, color='black')
-ax.plot(x, y, linestyle='solid', color='black')
-ax.plot(x, 1-y, linestyle='solid', color='black')
+xG = 1/(1+np.exp(b[0]+b[1]*shots['Angle'])) 
+shots = shots.assign(xG = xG)
+shots_40 = shots.iloc[:40]
+fig, ax = plt.subplots()
+ax.plot(shots_40['Angle']*180/np.pi, shots_40['Goal'], linestyle='none', marker= '.', markersize= 12, color='black', zorder = 3)
+ax.plot(x, y, linestyle='solid', color='black', zorder = 2)
+ax.plot(x, 1-y, linestyle='solid', color='black', zorder = 2)
 loglikelihood=0
 for item,shot in shots_40.iterrows():
-    ang=shot['Angle']*180/np.pi
-    if shot['Goal']==1:
-        loglikelihood=loglikelihood+np.log(shot['xG'])
-        ax.plot([ang,ang],[shot['Goal'],shot['xG']], color='red')
+    ang = shot['Angle'] * 180/np.pi
+    if shot['Goal'] == 1:
+        loglikelihood = loglikelihood + np.log(shot['xG'])
+        ax.plot([ang,ang],[shot['Goal'],shot['xG']], color='red', zorder = 1)
     else:
-        loglikelihood=loglikelihood+np.log(1 - shot['xG'])
-        ax.plot([ang,ang],[shot['Goal'],1-shot['xG']], color='blue')
+        loglikelihood = loglikelihood + np.log(1 - shot['xG'])
+        ax.plot([ang,ang], [shot['Goal'], 1-shot['xG']], color='blue', zorder = 1)
     
 ax.set_ylabel('Goal scored')
 ax.set_xlabel("Shot angle (degrees)")
 plt.ylim((-0.05,1.05))
-plt.xlim((0,80))
+plt.xlim((0,180))
 plt.text(45,0.2,'Log-likelihood:') 
 plt.text(45,0.1,str(loglikelihood))
 ax.set_yticks([0,1])
@@ -198,14 +206,12 @@ plt.show()
 #Make single variable model of angle
 #Using logistic regression we find the optimal values of b
 #This process minimizes the loglikelihood
-test_model = smf.glm(formula="Goal ~ Angle" , data=shots_model, 
+test_model = smf.glm(formula="Goal ~ Angle" , data=shots, 
                            family=sm.families.Binomial()).fit()
 print(test_model.summary())        
 b=test_model.params
-
-
-xGprob=1/(1+np.exp(b[0]+b[1]*midangle*np.pi/180)) 
-fig,ax=plt.subplots(num=1)
+xGprob = 1/(1+np.exp(b[0]+b[1]*midangle*np.pi/180)) 
+fig,ax=plt.subplots()
 ax.plot(midangle, prob_goal, linestyle='none', marker= '.', markersize= 12, color='black')
 ax.plot(midangle, xGprob, linestyle='solid', color='black')
 ax.set_ylabel('Probability chance scored')
@@ -214,18 +220,14 @@ ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 plt.show()
 
-
-
-
-
 #Now lets look at distance from goal
 
 
 #Show empirically how distance from goal predicts probability of scoring
-shotcount_dist=np.histogram(shots_model['Distance'],bins=40,range=[0, 70])
-goalcount_dist=np.histogram(goals_only['Distance'],bins=40,range=[0, 70])
-prob_goal=np.divide(goalcount_dist[0],shotcount_dist[0])
-distance=shotcount_dist[1]
+shotcount_dist = np.histogram(shots['Distance'],bins=40,range=[0, 70])
+goalcount_dist = np.histogram(goals['Distance'],bins=40,range=[0, 70])
+prob_goal = np.divide(goalcount_dist[0],shotcount_dist[0])
+distance = shotcount_dist[1]
 middistance= (distance[:-1] + distance[1:])/2
 fig,ax=plt.subplots(num=1)
 ax.plot(middistance, prob_goal, linestyle='none', marker= '.', color='black')
@@ -236,7 +238,7 @@ ax.spines['right'].set_visible(False)
 
 
 #Make single variable model of distance
-test_model = smf.glm(formula="Goal ~ Distance" , data=shots_model, 
+test_model = smf.glm(formula="Goal ~ Distance" , data=shots, 
                            family=sm.families.Binomial()).fit()
 print(test_model.summary())        
 b=test_model.params
@@ -248,8 +250,8 @@ plt.show()
 
 
 #Adding distance squared
-squaredD = shots_model['Distance']**2
-shots_model = shots_model.assign(D2=squaredD)
+squaredD = shots['Distance']**2
+shots_model = shots.assign(D2=squaredD)
 test_model = smf.glm(formula="Goal ~ Distance + D2" , data=shots_model, 
                            family=sm.families.Binomial()).fit()
 print(test_model.summary())        
@@ -266,17 +268,13 @@ plt.show()
 
 
 #Adding even more variables to the model.
-squaredX = shots_model['X']**2
-shots_model = shots_model.assign(X2=squaredX)
-squaredC = shots_model['C']**2
-shots_model = shots_model.assign(C2=squaredC)
-AX = shots_model['Angle']*shots_model['X']
-shots_model = shots_model.assign(AX=AX)
-
+shots["X2"] = shots['X']**2
+shots["C2"] = shots['C']**2
+shots["AX"]  = shots['Angle']*shots['X']
 
 # A general model for fitting goal probability
 # List the model variables you want here
-model_variables = ['Angle','Distance','X','C']
+model_variables = ['Angle','Distance','X','C', "X2", "C2", "AX"]
 model=''
 for v in model_variables[:-1]:
     model = model  + v + ' + '
@@ -284,7 +282,7 @@ model = model + model_variables[-1]
 
 
 #Fit the model
-test_model = smf.glm(formula="Goal ~ " + model, data=shots_model, 
+test_model = smf.glm(formula="Goal ~ " + model, data=shots, 
                            family=sm.families.Binomial()).fit()
 print(test_model.summary())        
 b=test_model.params
@@ -299,42 +297,43 @@ def calculate_xG(sh):
    return xG   
 
 #Add an xG to my dataframe
-xG=shots_model.apply(calculate_xG, axis=1) 
-shots_model = shots_model.assign(xG=xG)
+xG=shots.apply(calculate_xG, axis=1) 
+shots = shots.assign(xG=xG)
 
 
 #Create a 2D map of xG
-pgoal_2d=np.zeros((65,65))
-for x in range(65):
-    for y in range(65):
+pgoal_2d=np.zeros((68,68))
+for x in range(68):
+    for y in range(68):
         sh=dict()
-        a = np.arctan(7.32 *x /(x**2 + abs(y-65/2)**2 - (7.32/2)**2))
+        a = np.arctan(7.32 *x /(x**2 + abs(y-68/2)**2 - (7.32/2)**2))
         if a<0:
             a = np.pi + a
         sh['Angle'] = a
-        sh['Distance'] = np.sqrt(x**2 + abs(y-65/2)**2)
-        sh['D2'] = x**2 + abs(y-65/2)**2
+        sh['Distance'] = np.sqrt(x**2 + abs(y-68/2)**2)
+        sh['D2'] = x**2 + abs(y-68/2)**2
         sh['X'] = x
         sh['AX'] = x*a
         sh['X2'] = x**2
-        sh['C'] = abs(y-65/2)
-        sh['C2'] = (y-65/2)**2
+        sh['C'] = abs(y-68/2)
+        sh['C2'] = (y-68/2)**2
         
         pgoal_2d[x,y] =  calculate_xG(sh)
 
-(fig,ax) = FCPython.createGoalMouth()
-pos=ax.imshow(pgoal_2d, extent=[-1,65,65,-1], aspect='auto',cmap=plt.cm.Reds,vmin=0, vmax=0.3)
+pitch = VerticalPitch(line_color='black', half = True, pitch_type='custom', pitch_length=105, pitch_width=68, line_zorder = 2)
+fig, ax = pitch.draw()
+pos = ax.imshow(pgoal_2d, extent=[-1,68,68,-1], aspect='auto',cmap=plt.cm.Reds,vmin=0, vmax=0.3, zorder = 1)
 fig.colorbar(pos, ax=ax)
 ax.set_title('Probability of goal')
-plt.xlim((0,66))
-plt.ylim((-3,35))
+plt.xlim((0,68))
+plt.ylim((-1,35))
 plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
 
 ## Testing model fit
 
 # Mcfaddens Rsquared for Logistic regression
-null_model = smf.glm(formula="Goal ~ 1 ", data=shots_model,
+null_model = smf.glm(formula="Goal ~ 1 ", data=shots,
                      family=sm.families.Binomial()).fit()
 1 - test_model.llf / null_model.llf
 
